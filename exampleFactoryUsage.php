ba7991490f31a483256ee65d8ae7f941c3b9eda4
achmadJuliarman <acmadjuliarman@gmail.com>
2024-08-05 17:07:28 +0700

    hapus semuanya

--- exampleFactoryUsage.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'BookFactory.php';
-
-$book = BookFactory::createBook("Design Patterns", "Erich Gamma");
-echo $book->getDetails(); // Output: Title: Design Patterns, Author: Erich Gamma
-?>
--- exampleTemplateUsage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'TemplateMethod.php';
-
-$processor = new JSONBookProcessor();
-$processor->processBook(); // Output: Fetching book from JSON source.
-                           //         Processing JSON book details.
-                           //         Saving book to database.
-?>
